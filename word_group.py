042fdd727f0febb3ab94ca37f4574af05c5bbcfd
brettallenyo <bzallen@google.com>
2022-05-12 17:49:45 -0400

    fixed

--- word_group.py
+++ word_group.py
@@ -2,7 +2,7 @@ import argparse
 import numpy as np
 from utils import text_helper
 
-def group_words(input_dirs):
+def main(input_dirs):
     qst_vocab = text_helper.VocabDict(input_dir+'/vocab_questions.txt')
     
     print(qst_vocab)
@@ -13,3 +13,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--input_dir', type=str, default='./datasets',
                         help='input directory for visual question answering.')
+    
+    args = parser.parse_args()
+    
+    main(args)
